77101169cf24d589e4d851c199e43fdf2a6f1e04
fresaletlora <mikhaelfresa@gmail.com>
2021-11-18 00:15:15 +0700

    upload

---
+++ app/Http/Controllers/Admin/FraudController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\FraudModel;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+
+
+class FraudController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+
+          if (request()->ajax())
+        {
+
+            $query = FraudModel::with(['user']); //ini kalau terdapat relasi di database, namanya liat di modelnya pada saat buat relasi
+            return DataTables::of($query)
+            ->addIndexColumn()
+                ->addColumn('action',function($fraud){
+                        return '
+                            <div class ="btn-group">
+                                <div class="dropdown">
+                                   <a href = "'.route('fraud-detail',$fraud->no_kasus).'">
+                                   <button class="btn btn-primary dropdown-toggle mr-1 mb-1" type="button" data-toggle="dropdown">
+                                    Approval
+                                    </button>
+                                   </a>
+                                </div>
+                            </div>
+                        ';
+                })
+
+
+                 ->rawColumns(['action'])
+                ->make();
+
+        }
+
+        return view('admin.fraud_table');
+        // return User::with('fraud')->get();
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = FraudModel::with('user')->where('no_kasus',$id)->firstOrFail();
+
+        return view('admin.approval_fraud',['data'=>$data]);
+
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,75 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CustomerDisputeController as AdminCustomerDisputeController;
+use App\Http\Controllers\Admin\FraudController as AdminFraudController;
+use App\Http\Controllers\Admin\OtherController as AdminOtherController;
+use App\Http\Controllers\Admin\OutstandingController as AdminOutstandingController;
+use App\Http\Controllers\CustomerDisputeController;
+use App\Http\Controllers\FraudController;
+use App\Http\Controllers\OutstandingController;
+use App\Http\Controllers\OtherController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Models\Fraud;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PermitController;
+use App\Http\Controllers\LitigationController;
+use App\Models\CustomerDisputeModel;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+
+Route::get('/litigation', [LitigationController::class,'index']);
+
+
+Route::get('/litigation/fraud', [FraudController::class,'index']);
+Route::post('/litigation/fraud', [FraudController::class,'store']);
+
+Route::get('/litigation/customer-dispute',[CustomerDisputeController::class,'index']);
+Route::post('/litigation/customer-dispute',[CustomerDisputeController::class,'store']);
+Route::get('/litigation/outstanding', [OutstandingController::class,'index']);
+Route::post('/litigation/outstanding/input', [OutstandingController::class,'store']);
+Route::get('/litigation/other', [OtherController::class,'index']);
+Route::post('/litigation/other/input', [OtherController::class,'store']);
+
+Route::get('/permit/perizinanbaru', [PermitController::class,'index']);
+Route::post('/permit/perizinanbaru/input', [PermitController::class,'store']);
+
+Route::get('/login', [LoginController::class,'index']);
+Route::post('/login', [LoginController::class,'login']);
+Route::post('/logout', [LoginController::class,'logout']);
+Route::get('/register', [RegisterController::class,'index']);
+Route::post('/register', [RegisterController::class,'register']);
+// Route::get('/getCoba',[FraudController::class,'show']);
+
+// Route::get('/getCobaDetail/{id}',[FraudController::class,'getbyId'])->name('cobaDetail');
+
+Route::get('/admin/dashboard',[AdminController::class,'index']);
+
+Route::get('/admin/dashboard/customer-dispute',[AdminCustomerDisputeController::class,'index']);
+Route::get('/admin/dashboard/customer-dispute/detail/{id}',[AdminFraudController::class,'show'])->name('customer-dispute-detail');
+
+Route::get('/admin/dashboard/fraud',[AdminFraudController::class,'index']);
+Route::get('/admin/dashboard/fraud/detail/{id}',[AdminFraudController::class,'show'])->name('fraud-detail');
+
+Route::get('/admin/dashboard/outstanding',[AdminOutstandingController::class,'index']);
+Route::get('/admin/dashboard/outstanding/detail/{id}',[AdminOutstandingController::class,'show'])->name('outstanding-detail');
+
+Route::get('/admin/dashboard/other',[AdminOtherController::class,'index']);
+Route::get('/admin/dashboard/other/detail/{id}',[AdminOtherController::class,'show'])->name('other-detail');
